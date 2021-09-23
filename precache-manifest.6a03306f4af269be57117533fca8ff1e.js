52d7940f11aec9cf15fee90623cd39610e090d47
Code Sandbox <hello@codesandbox.io>
2021-09-23 17:42:54 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.6a03306f4af269be57117533fca8ff1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34a10f4e4c6bf884f25e0be83e2dfe46",
+    "url": "https://gabriel19971029.github.io/csb-cyv9w/index.html"
+  },
+  {
+    "revision": "b9bf536330fa027f9651",
+    "url": "https://gabriel19971029.github.io/csb-cyv9w/static/js/2.a4839814.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "https://gabriel19971029.github.io/csb-cyv9w/static/js/2.a4839814.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f79242e593ba7f4cf19",
+    "url": "https://gabriel19971029.github.io/csb-cyv9w/static/js/main.3a87a58c.chunk.js"
+  },
+  {
+    "revision": "da52b210c25e81efdec3",
+    "url": "https://gabriel19971029.github.io/csb-cyv9w/static/js/runtime-main.c789d8fb.js"
+  },
+  {
+    "revision": "657115616d5d27260157caf48f79b4b5",
+    "url": "https://gabriel19971029.github.io/csb-cyv9w/static/media/image.65711561.png"
+  }
+]);
